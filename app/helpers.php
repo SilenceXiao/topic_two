187a78c0ec98307c142edfcd67ebada349ac804e
rabbit <1282288696@qq.com>
2021-03-01 06:07:11 +0000

    test

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,7 @@ function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
+        dd($url);
 
         return $db_config = [
             'connection' => 'pgsql',
