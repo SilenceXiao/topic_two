a7bace49fa0a80de47852ee8e41e4a6b5d7a2436
rabbit <1282288696@qq.com>
2021-03-01 06:36:08 +0000

    test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,6 +35,8 @@ class UsersController extends Controller
         ]);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
-        return view('users.show',['user' => $user]);
+        // return view('users.show',['user' => $user]);
+        // return view('users.show',['user' => $user]);
+        return redirect()->route('users.show', [$user]);
     }
 }
